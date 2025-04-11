d7f2751e1d874cf1503b61b13cf1c1790e96d9f9
allen <allenmuhani@gmail.com>
2025-04-11 23:57:03 +0300

    configured next.config.ts for gh pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  assetPrefix: process.env.GITHUB_PAGES ? '/photo-store' : '',
+  basePath:  process.env.GITHUB_PAGES ? '/photo-store' : '',
+  output: "export",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    webpackBuildWorker: true,
+    parallelServerBuildTraces: true,
+    parallelServerCompiles: true,
+  },
 };
 
 export default nextConfig;
